3e3d5a70eea58227cc5f32e841ee389a954c6c87
joy1418 <1641581541@qq.com>
2024-03-31 17:40:06 +0800

    修复输出bug

--- huawei/src/main/java/com.example/Qes1.java
+++ huawei/src/main/java/com.example/Qes1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Qes1{
     public static void main(String[] args){
-        System.out.println(123);
+
         Scanner in = new Scanner(System.in);
         while (in.hasNextLine()) {
             String str =in.nextLine();

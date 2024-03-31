12c200d03a917b6790e023227d764e94f925fd7e
joy1418 <1641581541@qq.com>
2024-03-31 20:15:54 +0800

    第10题

--- huawei/src/main/java/com.example/Qes10.java
+++ huawei/src/main/java/com.example/Qes10.java
@@ -1,13 +1,24 @@
 package com.example;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Qes10 {
 
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         while (in.hasNextLine()){
-
+            String str = in.nextLine();
+            Set<Character> set = new HashSet<>();
+            int count = 0;
+            for(int i=0;i<str.length();i++){
+                if(!set.contains(str.charAt(i))){
+                    count++;
+                    set.add(str.charAt(i));
+                }
+            }
+            System.out.println(count);
         }
     }
 

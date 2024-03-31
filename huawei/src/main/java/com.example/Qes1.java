af07f8fa4ba61b30a06237bfeab4b11c107cdfe7
joy1418 <1641581541@qq.com>
2024-03-31 17:25:41 +0800

    模拟提交

--- huawei/src/main/java/com.example/Qes1.java
+++ huawei/src/main/java/com.example/Qes1.java
@@ -1,7 +1,15 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Qes1{
-    public static void main(Strngs[] args){
+    public static void main(String[] args){
         System.out.println(123);
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()) {
+            String str =in.nextLine();
+            String[] arr =str.split(" ");
+            System.out.println(arr[arr.length-1]);
+        }
     }
 }

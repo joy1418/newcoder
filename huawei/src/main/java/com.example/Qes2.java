1d1158086e128bd816c3f385ff3c3df682a1c664
joy1418 <1641581541@qq.com>
2024-03-31 17:40:21 +0800

    第二题

--- huawei/src/main/java/com.example/Qes2.java
+++ huawei/src/main/java/com.example/Qes2.java
@@ -6,7 +6,17 @@ public class Qes2 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         while (in.hasNextLine()){
-
+            String str1 = in.nextLine();
+            String str2 = in.nextLine();
+            str1=str1.toLowerCase();
+            str2=str2.toLowerCase();
+            int count=0;
+            for(int i=0;i<str1.length();i++){
+                if(str1.substring(i,i+1).equals(str2)){
+                    count++;
+                }
+            }
+            System.out.println(count);
         }
     }
 }

bdc1b3ed71ccdd4ffaea61e57a41693465249e42
joy1418 <1641581541@qq.com>
2024-04-01 19:42:04 +0800

    第78-80题

--- huawei/src/main/java/com.example/Qes79.java
+++ huawei/src/main/java/com.example/Qes79.java
@@ -1,11 +1,8 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
-public class Qes78 {
+public class Qes79 {
 
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
---
+++ huawei/src/main/java/com.example/Qes80.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Qes80 {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()){
+            int num1 = Integer.parseInt(in.nextLine());
+            String str1 = in.nextLine();
+            int num2 = Integer.parseInt(in.nextLine());
+            String str2 = in.nextLine();
+            List<Integer> list = new ArrayList<>();
+            String[] arr1 = str1.split(" ");
+            for (String s : arr1) {
+                if (!list.contains(Integer.parseInt(s))) {
+                    list.add(Integer.parseInt(s));
+                }
+            }
+            String[] arr2 = str2.split(" ");
+            for (String s : arr2) {
+                if (!list.contains(Integer.parseInt(s))) {
+                    list.add(Integer.parseInt(s));
+                }
+            }
+
+
+            Collections.sort(list);
+            for (int i = 0; i < list.size(); i++) {
+                System.out.print(list.get(i));
+            }
+        }
+    }
+}
